d19a6dc3ec03732bff34c9187674381146289248
Gabriel Leite <gabriel.leite@zup.com.br>
2018-11-26 16:36:40 -0200

    Code Smell: Empty Construct

--- src/main/java/com/zup/br/customerapi/pagination/PageImplementation.java
+++ src/main/java/com/zup/br/customerapi/pagination/PageImplementation.java
@@ -6,6 +6,9 @@ import java.util.HashMap;
 
 public class PageImplementation {
 
+    private PageImplementation() {        
+    }
+
     public static JSONPage loadPage(Page<?> page, String name) {
 
         JSONPage jsonPage = new JSONPage();
